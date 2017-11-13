f4ed77b463bb25f4cc00277245da36fafaf8da41
Ken Ren <renzhengyu@yahoo.com>
2017-11-12 16:40:14 -0800

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            /*
+            Console.WriteLine("Hello World!");
+            */
+            Console.WriteLine("\nHello,{0}, on {1:d} at {1:t}", name, date);
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
